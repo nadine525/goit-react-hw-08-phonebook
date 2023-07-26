b872337468a08db198644bfb2327d7af272153dd
nadine525 <02_seeders_wide@icloud.com>
2023-07-27 00:03:30 +0200

    logout

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,14 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { UserMenuDiv, Button, Paragraf } from './UserMenu.styled';
 import { selectUser } from 'redux/auth/selectors';
+import { logOutUser } from 'redux/auth/operations';
+import defaultAvatar from './images.png';
+
+const styles = {
+  avatar: {
+    borderRadius: 50,
+  },
+};
 
 const UserMenu = () => {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+  const avatar = defaultAvatar;
 
   return (
     <UserMenuDiv>
-      <Paragraf>Welcome, {user.name}</Paragraf>
-      <Button type="button">LogOut</Button>
+      <img src={avatar} alt="avatar" width={30} style={styles.avatar} />
+      <Paragraf>Welcome, {user.name} </Paragraf>
+      <Button type="button" onClick={() => dispatch(logOutUser())}>
+        LogOut
+      </Button>
     </UserMenuDiv>
   );
 };
