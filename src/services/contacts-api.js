62141261bace573a7eb6f2005a48d5398442e9ad
nadine525 <02_seeders_wide@icloud.com>
2023-07-27 22:56:53 +0200

    rv

--- src/services/contacts-api.js
+++ src/services/contacts-api.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 export const fetchContacts = async () => {
-  const response = await axios.get(`/contacts`);
+  const response = await axios.get('/contacts);
   return response.data;
 };
 
 console.log(fetchContacts());
 
 export const addContact = async newContact => {
-  const response = await axios.post(`/contacts`, newContact);
+  const response = await axios.post('/contacts', newContact);
   return response.data;
 };
 
